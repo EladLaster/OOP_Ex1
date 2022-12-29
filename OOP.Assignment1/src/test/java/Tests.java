7523d74efa7f6bc8b85620d247a7826c96319dec
EladLaster <118683420+EladLaster@users.noreply.github.com>
2022-12-29 15:41:46 +0200

    Update Tests.java

--- OOP.Assignment1/src/test/java/Tests.java
+++ OOP.Assignment1/src/test/java/Tests.java
@@ -18,4 +18,40 @@ public class Tests {
 
         logger.info(() -> JvmUtilities.jvmInfo());
     }
+    @Test
+    public void testNotify() {
+        UndoableStringBuilder usb = new UndoableStringBuilder();
+        usb.append("David Stern and Elad Laster");
+        ga.register(cm);
+        ga.register(cm2);
+        ga.append("to be or to be");
+        assertEquals("to be or to be", cm.toString());
+        ga.Notify(usb);
+        for (int i = 0; i < ga.arrMember.size(); i++) {
+            assertEquals("David Stern and Elad Laster", (ga.arrMember.get(i)).toString());
+
+            logger.info(() -> JvmUtilities.objectFootprint(cm, cm2));
+            logger.info(() -> "memory size in member after Notify:");
+            logger.info(() -> JvmUtilities.objectTotalSize(cm));
+            logger.info(() -> JvmUtilities.objectTotalSize(cm2));
+            logger.info(() -> JvmUtilities.jvmInfo());
+        }
+    }
+        @Test
+        public void testUpdate () {
+            UndoableStringBuilder usb = new UndoableStringBuilder();
+            GroupAdmin ga = new GroupAdmin();
+            ConcreteMember cm = new ConcreteMember();
+            ga.register(cm);
+            ga.append("elad laster");
+            assertEquals("elad laster", cm.toString());
+            usb.append("David Stern");
+            cm.update(usb);
+            assertEquals("David Stern", cm.toString());
+
+            logger.info(() -> JvmUtilities.objectFootprint(cm));
+            logger.info(() -> "memory size in member after update:");
+            logger.info(() -> JvmUtilities.objectTotalSize(cm));
+            logger.info(() -> JvmUtilities.jvmInfo());
+        }
 }
