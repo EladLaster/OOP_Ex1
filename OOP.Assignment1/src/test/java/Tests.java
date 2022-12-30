04c778e181649a301ef98ecda205aa77c60089c9
EladLaster <118683420+EladLaster@users.noreply.github.com>
2022-12-30 14:04:58 +0200

    Update Tests.java

--- OOP.Assignment1/src/test/java/Tests.java
+++ OOP.Assignment1/src/test/java/Tests.java
@@ -1,4 +1,4 @@
-import observer.ConcreteMember;
+ver.ConcreteMember;
 import observer.GroupAdmin;
 import observer.UndoableStringBuilder;
 import org.junit.jupiter.api.Test;
@@ -31,15 +31,19 @@ public class Tests {
 
     @Test
     public void testRegister() {
+        System.out.println("memory in group before register:");
+        logger.info(() -> JvmUtilities.objectFootprint(ga));
+        logger.info(() -> JvmUtilities.objectTotalSize(ga));
+        logger.info(() -> JvmUtilities.jvmInfo());
+
         ga.register(cm);
         assertEquals(1, ga.arrMember.size());
         ga.register(cm2);
         assertEquals(2, ga.arrMember.size());
 
-        logger.info(() -> JvmUtilities.objectFootprint(cm, cm2));
-        logger.info(() -> "memory size in member after register:");
-        logger.info(() -> JvmUtilities.objectTotalSize(cm));
-        logger.info(() -> JvmUtilities.objectTotalSize(cm2));
+        System.out.println("memory in group after register:");
+        logger.info(() -> JvmUtilities.objectFootprint(ga));
+        logger.info(() -> JvmUtilities.objectTotalSize(ga));
         logger.info(() -> JvmUtilities.jvmInfo());
     }
 
@@ -47,15 +51,20 @@ public class Tests {
     public void testUnregister() {
         ga.register(cm);
         ga.register(cm2);
+
+        System.out.println("memory in group before unregister:");
+        logger.info(() -> JvmUtilities.objectFootprint(ga));
+        logger.info(() -> JvmUtilities.objectTotalSize(ga));
+        logger.info(() -> JvmUtilities.jvmInfo());
+
         ga.unregister(cm);
         assertEquals(1, ga.arrMember.size());
         ga.unregister(cm2);
         assertEquals(0, ga.arrMember.size());
 
-        logger.info(() -> JvmUtilities.objectFootprint(cm, cm2));
-        logger.info(() -> "memory size in member after unregister:");
-        logger.info(() -> JvmUtilities.objectTotalSize(cm));
-        logger.info(() -> JvmUtilities.objectTotalSize(cm2));
+        System.out.println("memory in group after unregister:");
+        logger.info(() -> JvmUtilities.objectFootprint(ga));
+        logger.info(() -> JvmUtilities.objectTotalSize(ga));
         logger.info(() -> JvmUtilities.jvmInfo());
 
     }
@@ -64,11 +73,17 @@ public class Tests {
     @Test
     void testAppend() {
         ga.register(cm);
+
+        System.out.println("memory  in member before append:");
+        logger.info(() -> JvmUtilities.objectFootprint(cm));
+        logger.info(() -> JvmUtilities.objectTotalSize(cm));
+        logger.info(() -> JvmUtilities.jvmInfo());
+
         ga.append("hello");
         assertEquals("hello", cm.toString());
 
+        logger.info(() -> "memory in member after append:");
         logger.info(() -> JvmUtilities.objectFootprint(cm));
-        logger.info(() -> "memory size in member after append:");
         logger.info(() -> JvmUtilities.objectTotalSize(cm));
         logger.info(() -> JvmUtilities.jvmInfo());
 
@@ -78,11 +93,17 @@ public class Tests {
     void testDelete() {
         ga.register(cm);
         ga.append("hello world");
+
+        System.out.println("memory in member before delete:");
+        logger.info(() -> JvmUtilities.objectFootprint(cm));
+        logger.info(() -> JvmUtilities.objectTotalSize(cm));
+        logger.info(() -> JvmUtilities.jvmInfo());
+
         ga.delete(0, 6);
         assertEquals("world", cm.toString());
 
+        System.out.println("memory in member after delete:");
         logger.info(() -> JvmUtilities.objectFootprint(cm));
-        logger.info(() -> "memory size in member after delete:");
         logger.info(() -> JvmUtilities.objectTotalSize(cm));
         logger.info(() -> JvmUtilities.jvmInfo());
     }
@@ -91,11 +112,17 @@ public class Tests {
     void testInsert() {
         ga.register(cm);
         ga.append("world");
+
+        System.out.println("memory in member before insert:");
+        logger.info(() -> JvmUtilities.objectFootprint(cm));
+        logger.info(() -> JvmUtilities.objectTotalSize(cm));
+        logger.info(() -> JvmUtilities.jvmInfo());
+
         ga.insert(0, "hello ");
         assertEquals("hello world", cm.toString());
 
+        System.out.println("memory in member after insert:");
         logger.info(() -> JvmUtilities.objectFootprint(cm));
-        logger.info(() -> "memory size in member after insert:");
         logger.info(() -> JvmUtilities.objectTotalSize(cm));
         logger.info(() -> JvmUtilities.jvmInfo());
     }
@@ -105,12 +132,24 @@ public class Tests {
     void testUndo() {
         ga.register(cm);
         ga.append("world");
+
+        System.out.println("memory in member before the second append:");
+        logger.info(() -> JvmUtilities.objectFootprint(cm));
+        logger.info(() -> JvmUtilities.objectTotalSize(cm));
+        logger.info(() -> JvmUtilities.jvmInfo());
+
         ga.append("s");
+
+        System.out.println("memory in member after the second append:");
+        logger.info(() -> JvmUtilities.objectFootprint(cm));
+        logger.info(() -> JvmUtilities.objectTotalSize(cm));
+        logger.info(() -> JvmUtilities.jvmInfo());
+
         ga.undo();
         assertEquals("world", cm.toString());
 
+        System.out.println("memory in member after undo:");
         logger.info(() -> JvmUtilities.objectFootprint(cm));
-        logger.info(() -> "memory size in member after undo:");
         logger.info(() -> JvmUtilities.objectTotalSize(cm));
         logger.info(() -> JvmUtilities.jvmInfo());
     }
@@ -121,18 +160,33 @@ public class Tests {
         usb.append("David Stern and Elad Laster");
         ga.register(cm);
         ga.register(cm2);
+
+        System.out.println("memory in member before Notify:");
+        logger.info(() -> JvmUtilities.objectFootprint(cm));
+        logger.info(() -> JvmUtilities.objectTotalSize(cm));
+        logger.info(() -> JvmUtilities.jvmInfo());
+
+        System.out.println("memory in group before Notify:");
+        logger.info(() -> JvmUtilities.objectFootprint(ga));
+        logger.info(() -> JvmUtilities.objectTotalSize(ga));
+        logger.info(() -> JvmUtilities.jvmInfo());
+
         ga.append("to be or to be");
         assertEquals("to be or to be", cm.toString());
         ga.Notify(usb);
         for (int i = 0; i < ga.arrMember.size(); i++) {
             assertEquals("David Stern and Elad Laster", (ga.arrMember.get(i)).toString());
-
-            logger.info(() -> JvmUtilities.objectFootprint(cm, cm2));
-            logger.info(() -> "memory size in member after Notify:");
-            logger.info(() -> JvmUtilities.objectTotalSize(cm));
-            logger.info(() -> JvmUtilities.objectTotalSize(cm2));
-            logger.info(() -> JvmUtilities.jvmInfo());
         }
+        System.out.println("memory in member after Notify:");
+        logger.info(() -> JvmUtilities.objectFootprint(cm));
+        logger.info(() -> JvmUtilities.objectTotalSize(cm));
+        logger.info(() -> JvmUtilities.jvmInfo());
+
+        System.out.println("memory in group after Notify:");
+        logger.info(() -> JvmUtilities.objectFootprint(ga));
+        logger.info(() -> JvmUtilities.objectTotalSize(ga));
+        logger.info(() -> JvmUtilities.jvmInfo());
+
     }
         @Test
         public void testUpdate () {
@@ -140,14 +194,20 @@ public class Tests {
             GroupAdmin ga = new GroupAdmin();
             ConcreteMember cm = new ConcreteMember();
             ga.register(cm);
+
+            System.out.println("memory in member before update:");
+            logger.info(() -> JvmUtilities.objectFootprint(cm));
+            logger.info(() -> JvmUtilities.objectTotalSize(cm));
+            logger.info(() -> JvmUtilities.jvmInfo());
+
             ga.append("elad laster");
             assertEquals("elad laster", cm.toString());
             usb.append("David Stern");
             cm.update(usb);
             assertEquals("David Stern", cm.toString());
 
+            System.out.println("memory in member after update:");
             logger.info(() -> JvmUtilities.objectFootprint(cm));
-            logger.info(() -> "memory size in member after update:");
             logger.info(() -> JvmUtilities.objectTotalSize(cm));
             logger.info(() -> JvmUtilities.jvmInfo());
         }
